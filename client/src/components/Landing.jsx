fdbd4ed8877a7400ff9e93e7dc0af932f208c31b
sakshamsomra <77576187+sakshamsomra@users.noreply.github.com>
2023-05-11 14:20:48 +0530

    Add files via upload

---
+++ client/src/components/Landing.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import axios from "axios";
+import { useState, useRef, useEffect } from "react";
+import { Link, redirect, Route, useNavigate } from "react-router-dom";
+
+
+
+export default function Landing() {
+
+    const navigate = useNavigate();
+
+    axios.defaults.withCredentials = true;
+
+
+    const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
+
+    const submitForm = () => {
+
+        axios.post("http://sql12.freesqldatabase.com:3000/api/login", {
+
+            username: username,
+            password: password,
+
+        }).then((response) => {
+            if (response.data.message) {
+                alert(response.data.message);
+            }
+            else {
+                alert(" Successfull login " + username);
+                navigate("/home");
+            }
+        })
+
+    }
+
+    useEffect(() => {
+
+        axios.get("http://sql12.freesqldatabase.com:3000/api/login").then((response) => {
+            console.log(response);
+        })
+
+    }, [])
+
+
+
+    return (
+
+        <>
+
+            <div class="container1">
+                <div class="cards">
+                    <p class="singup">Log In</p>
+
+
+                    <div class="inputBox">
+                        <input type="text" required="required" onChange={(e) => {
+                            setUsername(e.target.value)
+                        }} />
+                        <span>Username</span>
+                    </div>
+
+                    <div class="inputBox">
+                        <input type="password" required="required" onChange={(e) => {
+                            setPassword(e.target.value)
+                        }} />
+                        <span>Password</span>
+                    </div>
+
+                    <button type="button" class="enter" onClick={submitForm}>Enter</button>
+
+                </div>
+            </div>
+
+
+
+        </>
+
+
+    )
+}
