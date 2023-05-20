799a03dffe525875c8acc1c4d677ce3b0bf2d7e0
sakshamsomra <77576187+sakshamsomra@users.noreply.github.com>
2023-05-21 00:51:06 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -15,7 +15,7 @@ export default function Navbar() {
     const [user, setUser] = useState([]);
 
     useEffect(() => {
-        axios.get("https://e-commerce-server-ewfplxmpn-sakshamsomra.vercel.app/api/username").then((response) => {
+        axios.get("https://e-commerce-server-lake.vercel.app/api/username").then((response) => {
 
 
             setUser(response.data);
@@ -27,7 +27,7 @@ export default function Navbar() {
 
     const log = () => {
 
-        axios.post("https://e-commerce-server-ewfplxmpn-sakshamsomra.vercel.app/api/logout", {
+        axios.post("https://e-commerce-server-lake.vercel.app/api/logout", {
 
 
         }).then((response) => {
