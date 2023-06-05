af69792bf0be220c04befb4c3711103ab2c7fa09
sakshamsomra <77576187+sakshamsomra@users.noreply.github.com>
2023-06-05 18:35:05 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -101,12 +101,7 @@ export default function Navbar() {
                             </li>
 
                         </ul>
-                        <form class="d-flex" role="search">
-                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                            <button class="btn btn-success">
-                                search
-                            </button>
-                        </form>
+                       
                     </div>
                 </div>
             </nav>
