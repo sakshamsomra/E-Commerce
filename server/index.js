9a587a2557d1bf5e15de441b12f133cd4f89134e
sakshamsomra <77576187+sakshamsomra@users.noreply.github.com>
2023-06-29 01:15:58 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -40,7 +40,7 @@ app.set('trust proxy', 1); // Enable trust proxy
 app.use(session({
   key: "userId",
   secret: 'secret',
-  resave: false,
+  resave: true,
   saveUninitialized: true,
   cookie: {
       secure: true,
